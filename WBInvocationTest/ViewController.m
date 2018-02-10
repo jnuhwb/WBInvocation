75eafc3790aa68e6913ab688c9876711ec5a5e68
wellbin <jnuhwb@163.com>
2018-02-10 21:33:25 +0800

    add WKWebView demo

--- WBInvocationTest/ViewController.m
+++ WBInvocationTest/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "NSObject+WBInvocation.h"
+#import <dlfcn.h>
 
 @interface ViewController ()
+
+@property (nonatomic) id webView;
 
 @end
 
@@ -17,6 +21,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+#if TARGET_IPHONE_SIMULATOR
+        NSString *frameworkPath = [[NSProcessInfo processInfo] environment][@"DYLD_FALLBACK_FRAMEWORK_PATH"];
+        if (frameworkPath) {
+            NSString *webkitLibraryPath = [NSString pathWithComponents:@[frameworkPath, @"WebKit.framework", @"WebKit"]];
+            dlopen([webkitLibraryPath cStringUsingEncoding:NSUTF8StringEncoding], RTLD_LAZY);
+        }
+#else
+        dlopen("/System/Library/Frameworks/WebKit.framework/WebKit", RTLD_LAZY);
+#endif
+    }
+        
+    id cfg = [NSClassFromString(@"WKWebViewConfiguration") new];
+    id userContentController = [NSClassFromString(@"WKUserContentController") new];
+    [userContentController invocationSelector:@"addScriptMessageHandler:name:", self, @"handler"];
+    [cfg setValue:userContentController forKey:@"userContentController"];
+    self.webView = [[NSClassFromString(@"WKWebView") alloc] invocationSelector:@"initWithFrame:configuration:" types:@[[NSString stringWithUTF8String:@encode(CGRect)], @"@"], self.view.bounds, cfg];
+    [self.view addSubview:self.webView];
+    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://github.com"]]];
 }
 
 
